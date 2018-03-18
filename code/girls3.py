ef0b2ddaca371079c4aa30f4f371a99552613304
Anisha Grover <anisha@Anishas-MacBook-Pro.local>
2018-03-17 17:40:24 -0700

    changed

---
+++ code/girls3.py
@@ -0,0 +1,20 @@
+'''
+Created on Mar 17, 2018
+
+@author: anisha
+'''
+
+my_number = 13
+
+# Ask the user to guess
+guess = int(input("Enter a guess: "))
+
+# Keep asking until the guess becomes equal to
+# the secret number is awesome
+while guess != my_number:
+    print "Guess again!"
+    guess = int(input("Enter a guess: "))
+15
+print "Good job, you got it!"
+
+
